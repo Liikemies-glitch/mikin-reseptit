0bd47cd0f54faad23cc505678db388e1ad09f3db
Mikki Aalto-Ylevä <mikki@goodside.fi>
2026-07-23 19:15:12 +0000

    Set Brownie portions to 2–6 servings

--- app.js
+++ app.js
@@ -260,7 +260,7 @@ function goBackToIndex() {
 }
 
 async function init() {
-  const res = await fetch("./recipes.json?v=46");
+  const res = await fetch("./recipes.json?v=47");
   state.data = await res.json();
 
   document.title = state.data.brand || state.data.title;
