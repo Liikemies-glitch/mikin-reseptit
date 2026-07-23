4b1d3d9639eb8c9eb44c303af8ccd9d2fe6b1c8a
Mikki Aalto-Ylevä <mikki@goodside.fi>
2026-07-23 18:35:13 +0000

    Simplify Creamy Beef Pasta pasta ingredient to Pastaa

--- app.js
+++ app.js
@@ -260,7 +260,7 @@ function goBackToIndex() {
 }
 
 async function init() {
-  const res = await fetch("./recipes.json");
+  const res = await fetch("./recipes.json?v=16");
   state.data = await res.json();
 
   document.title = state.data.brand || state.data.title;
