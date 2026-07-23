a9a37cee7b55c38fed7a70d8cac7de989550e7c1
Mikki Aalto-Ylevä <mikki@goodside.fi>
2026-07-23 19:24:23 +0000

    Clarify ice cream uses 400 g raspberries in two stages

--- app.js
+++ app.js
@@ -260,7 +260,7 @@ function goBackToIndex() {
 }
 
 async function init() {
-  const res = await fetch("./recipes.json?v=52");
+  const res = await fetch("./recipes.json?v=53");
   state.data = await res.json();
 
   document.title = state.data.brand || state.data.title;
