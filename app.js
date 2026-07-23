3d2f8ba00736499b0189f5682d98daa4e2829766
Mikki Aalto-Ylevä <mikki@goodside.fi>
2026-07-23 19:09:25 +0000

    Rate Mikin rahkakakku 6/10

--- app.js
+++ app.js
@@ -260,7 +260,7 @@ function goBackToIndex() {
 }
 
 async function init() {
-  const res = await fetch("./recipes.json?v=34");
+  const res = await fetch("./recipes.json?v=35");
   state.data = await res.json();
 
   document.title = state.data.brand || state.data.title;
