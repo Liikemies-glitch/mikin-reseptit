940dc35ef6082586e17ee7da70d9d2ce0e3e062d
Mikki Aalto-Ylevä <mikki@goodside.fi>
2026-07-23 19:14:05 +0000

    Set Rahkapohja-pizza portions to 1–2 servings

--- app.js
+++ app.js
@@ -260,7 +260,7 @@ function goBackToIndex() {
 }
 
 async function init() {
-  const res = await fetch("./recipes.json?v=43");
+  const res = await fetch("./recipes.json?v=44");
   state.data = await res.json();
 
   document.title = state.data.brand || state.data.title;
