791261eae788ca72038d33d302888005b3922914
Mikki Aalto-Ylevä <mikki@goodside.fi>
2026-07-23 19:15:54 +0000

    Set Vadelmapirtelö portion to 1 serving

--- app.js
+++ app.js
@@ -260,7 +260,7 @@ function goBackToIndex() {
 }
 
 async function init() {
-  const res = await fetch("./recipes.json?v=48");
+  const res = await fetch("./recipes.json?v=49");
   state.data = await res.json();
 
   document.title = state.data.brand || state.data.title;
